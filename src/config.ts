556ed44403508cc9cf66ec36e42e7269d80faee6
Elliana Petrin <103009442+EllianaPetrin@users.noreply.github.com>
2023-11-15 17:54:03 +0000

    testing out template

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@
 import type { Site, SocialMediaObjects } from "./types";
 
 export const SITE: Site = {
-    siteUrl: "https:/astro-lane.chrstnl.com/", // Always put "/" at the end of the URL
-    author: "chrstnl",
-    desc: "A personal portfolio landing template for developers and designers.",
-    title: "Astro Lane",
+    siteUrl: "https:/mysite.com/", // Always put "/" at the end of the URL
+    author: "ellie",
+    desc: "stardew valley",
+    title: "stardew valley",
     ogImage: "images/astro-lane.png",
     keywords:
         "Personal portfolio, landing page, page template, developer portfoliom designer portfolio",
@@ -35,7 +35,7 @@ export const SOCIALS: SocialMediaObjects = [
         href: "https://github.com/christian-luntok/astro-lane/",
         icon: "ph:instagram-logo-duotone",
         title: `Follow ${SITE.author} on Instagram`,
-        active: true
+        active: false
     },
     {
         name: "LinkedIn",
@@ -56,7 +56,7 @@ export const SOCIALS: SocialMediaObjects = [
         href: "https://github.com/christian-luntok/astro-lane/",
         icon: "ph:twitter-logo-duotone",
         title: `Follow ${SITE.author} on Twitter`,
-        active: true
+        active: false
     },
     {
         name: "YouTube",
---
+++ src/content/blog/new-post.md
@@ -0,0 +1,7 @@
+---
+title: "Astro Lane V1.0.0"
+description: "New post"
+pubDate: 2023-11-15
+---
+
+#new post
